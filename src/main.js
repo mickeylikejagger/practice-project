48a4e62a225433723d3d80965c0383879109adab
Michelle <143650720+mickeylikejagger@users.noreply.github.com>
2023-09-09 00:29:18 -0400

    created first java script function

---
+++ src/main.js
@@ -0,0 +1,11 @@
+const button = document.getElementById('button');
+const michelle = document.getElementById("hey");
+
+button.addEventListener('click', ()=>{
+    const hey = document.getElementById("hey");
+    hey.style.display === "none" ? hey.style.display = "block" :"none";
+});
+
+michelle.addEventListener('click', ()=>{
+    
+});
